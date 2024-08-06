d22a8aed6d370ab802090976902a043064a0d159
Yunus Emre ÖNEÇ <107077148+yunusonec@users.noreply.github.com>
2024-08-06 19:51:19 +0300

    manager

---
+++ OrderManagementSignalR.BusinessLayer/Concrete/CategoryManager.cs
@@ -0,0 +1,46 @@
+﻿using OrderManagementSignalR.BusinessLayer.Abstract;
+using OrderManagementSignalR.DataAccessLayer.Abstract;
+using OrderManagementSignalR.EntityLayer.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrderManagementSignalR.BusinessLayer.Concrete
+{
+    public class CategoryManager : ICategoryService
+    {
+        private readonly ICategoryDal _categoryDal;
+
+        public CategoryManager(ICategoryDal categoryDal)
+        {
+            _categoryDal = categoryDal;
+        }
+
+        public void TAdd(Category entity)
+        {
+            _categoryDal.Add(entity);
+        }
+
+        public void TDelete(Category entity)
+        {
+            _categoryDal.Delete(entity);
+        }
+
+        public Category TGetByID(int id)
+        {
+            return _categoryDal.GetByID(id);
+        }
+
+        public List<Category> TGetListAll()
+        {
+            return _categoryDal.GetListAll();
+        }
+
+        public void TUpdate(Category entity)
+        {
+            _categoryDal.Update(entity);
+        }
+    }
+}
