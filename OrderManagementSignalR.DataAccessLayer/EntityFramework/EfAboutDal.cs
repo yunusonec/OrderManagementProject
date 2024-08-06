2c8285f74817961d364081a432b8b0b9dff5392e
Yunus Emre ÖNEÇ <107077148+yunusonec@users.noreply.github.com>
2024-08-06 19:30:14 +0300

    Data Access

---
+++ OrderManagementSignalR.DataAccessLayer/EntityFramework/EfAboutDal.cs
@@ -0,0 +1,20 @@
+﻿using OrderManagementSignalR.DataAccessLayer.Abstract;
+using OrderManagementSignalR.DataAccessLayer.Concrete;
+using OrderManagementSignalR.DataAccessLayer.Repositories;
+using OrderManagementSignalR.EntityLayer.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrderManagementSignalR.DataAccessLayer.EntityFramework
+{
+    public class EfAboutDal : GenericRepository<About>, IAboutDal
+    {
+        public EfAboutDal(SignalRContext context) : base(context)
+        {
+
+        }
+    }
+}
---
+++ OrderManagementSignalR.DataAccessLayer/EntityFramework/EfContactDal.cs
@@ -0,0 +1,19 @@
+﻿using OrderManagementSignalR.DataAccessLayer.Abstract;
+using OrderManagementSignalR.DataAccessLayer.Concrete;
+using OrderManagementSignalR.DataAccessLayer.Repositories;
+using OrderManagementSignalR.EntityLayer.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrderManagementSignalR.DataAccessLayer.EntityFramework
+{
+    public class EfContactDal : GenericRepository<Contact>, IContactDal
+    {
+        public EfContactDal(SignalRContext context) : base(context)
+        {
+        }
+    }
+}
