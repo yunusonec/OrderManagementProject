7f2cd4b34ee51b14ccbceadcf37b1ff6ff362df8
Yunus Emre ÖNEÇ <107077148+yunusonec@users.noreply.github.com>
2024-08-09 18:18:38 +0300

    Dto Layer Bitti

---
+++ OrderManagementSignalR.DtoLayer/ContactDto/CreateContactDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrderManagementSignalR.DtoLayer.ContactDto
+{
+    public class CreateContactDto
+    {
+        public string Location { get; set; }
+        public string Phone { get; set; }
+        public string Mail { get; set; }
+    }
+}
---
+++ OrderManagementSignalR.DtoLayer/ContactDto/UpdateContactDto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrderManagementSignalR.DtoLayer.ContactDto
+{
+    public class UpdateContactDto
+    {
+        public int ContactID { get; set; }
+        public string Location { get; set; }
+        public string Phone { get; set; }
+        public string Mail { get; set; }
+    }
+}
