0099fb32acec4d2cf8c25bdbae6912ce442f053b
Yunus Emre ÖNEÇ <107077148+yunusonec@users.noreply.github.com>
2024-09-09 13:08:48 +0300

    Kategori Api test

--- OrderManagementSignalRApi/Program.cs
+++ OrderManagementSignalRApi/Program.cs
@@ -1,6 +1,18 @@
+using OrderManagementSignalR.BusinessLayer.Abstract;
+using OrderManagementSignalR.BusinessLayer.Concrete;
+using OrderManagementSignalR.DataAccessLayer.Abstract;
+using OrderManagementSignalR.DataAccessLayer.Concrete;
+using OrderManagementSignalR.DataAccessLayer.EntityFramework;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+builder.Services.AddDbContext<SignalRContext>();
+
+builder.Services.AddScoped<IAboutService, AboutManager>();
+builder.Services.AddScoped<IAboutDal,EfAboutDal>();
+
+builder.Services.AddScoped<IBookingService, BookingManager>();
+builder.Services.AddScoped<IBookingDal, EfBookingDal>();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
