3c55509ad3bedb6810483adb69d342d0954fa5fa
Yunus Emre ÖNEÇ <107077148+yunusonec@users.noreply.github.com>
2024-10-02 22:41:40 +0300

    Order ve Order Detail Sınıflarının Mimari Kısımları

---
+++ OrderManagementSignalR.BusinessLayer/Concrete/OrderManager.cs
@@ -0,0 +1,46 @@
+﻿using OrderManagementSignalR.BusinessLayer.Abstract;
+using OrderManagementSignalR.DataAccessLayer.Abstract;
+using OrderManagementSignalR.EntityLayer.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrderManagementSignalR.BusinessLayer.Concrete
+{
+    public class OrderManager : IOrderService
+    {
+        private readonly IOrderDal _orderDal;
+
+        public OrderManager(IOrderDal orderDal)
+        {
+            _orderDal = orderDal;
+        }
+
+        public void TAdd(Order entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void TDelete(Order entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Order TGetByID(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Order> TGetListAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void TUpdate(Order entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- OrderManagementSignalRApi/Program.cs
+++ OrderManagementSignalRApi/Program.cs
@@ -3,6 +3,7 @@ using OrderManagementSignalR.BusinessLayer.Concrete;
 using OrderManagementSignalR.DataAccessLayer.Abstract;
 using OrderManagementSignalR.DataAccessLayer.Concrete;
 using OrderManagementSignalR.DataAccessLayer.EntityFramework;
+using OrderManagementSignalR.EntityLayer.Entities;
 using OrderManagementSignalRApi.Hubs;
 using System.Reflection;
 
@@ -50,6 +51,12 @@ builder.Services.AddScoped<ISocialMediaDal, EfSocialMediaDal>();
 
 builder.Services.AddScoped<ITestimonialService, TestimonialManager>();
 builder.Services.AddScoped<ITestimonialDal, EfTestimonialDal>();
+
+builder.Services.AddScoped<IOrderService, OrderManager>();
+builder.Services.AddScoped<IOrderDal, EfOrderDal>();
+
+builder.Services.AddScoped<IOrderDetailService, OrderDetailManager>();
+builder.Services.AddScoped<IOrderDetailDal, EfOrderDetailDal>();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
