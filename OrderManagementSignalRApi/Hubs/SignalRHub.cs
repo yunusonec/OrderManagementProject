3d665ef8337c040fa20c20c31bc1c0befac43b3b
Yunus Emre ÖNEÇ <107077148+yunusonec@users.noreply.github.com>
2024-10-08 16:47:46 +0300

    ktif ve Pasif Kategori Sayılar

--- OrderManagementSignalRApi/Hubs/SignalRHub.cs
+++ OrderManagementSignalRApi/Hubs/SignalRHub.cs
@@ -1,16 +1,29 @@
 ﻿using Microsoft.AspNetCore.SignalR;
+using OrderManagementSignalR.BusinessLayer.Abstract;
 using OrderManagementSignalR.DataAccessLayer.Concrete;
 
 namespace OrderManagementSignalRApi.Hubs
 {
     public class SignalRHub : Hub
     {
-        SignalRContext context = new SignalRContext();
+        private readonly ICategoryService _categoryService;
+        private readonly IProductService _productService;
+
+        public SignalRHub(ICategoryService categoryService, IProductService productService)
+        {
+            _categoryService = categoryService;
+            _productService = productService;
+        }
 
         public async Task SendCategoryCount()
         {
-            var value = context.Categories.Count();
+            var value = _categoryService.TCategoryCount();
             await Clients.All.SendAsync("ReceiveCategoryCount", value);
+        }
+        public async Task SendProductCount() 
+        {
+            var value2= _productService.TProductCount();
+            await Clients.All.SendAsync("ReceiveProductCount", value2);
         }
     }
 }
