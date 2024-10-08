fb3320e7a365c69e8c7fba14edc8fa5c46450916
Yunus Emre ÖNEÇ <107077148+yunusonec@users.noreply.github.com>
2024-10-08 17:00:47 +0300

    Kategori İstatistiklerini SignalR'da Çekme

--- OrderManagementSignalRApi/Hubs/SignalRHub.cs
+++ OrderManagementSignalRApi/Hubs/SignalRHub.cs
@@ -15,15 +15,19 @@ namespace OrderManagementSignalRApi.Hubs
             _productService = productService;
         }
 
-        public async Task SendCategoryCount()
+        public async Task SendStatistic()
         {
             var value = _categoryService.TCategoryCount();
             await Clients.All.SendAsync("ReceiveCategoryCount", value);
-        }
-        public async Task SendProductCount() 
-        {
-            var value2= _productService.TProductCount();
+
+            var value2 = _productService.TProductCount();
             await Clients.All.SendAsync("ReceiveProductCount", value2);
+
+            var value3 = _categoryService.TActiveCategoryCount();
+            await Clients.All.SendAsync("ReceiveActiveCategoryCount", value3);
+
+            var value4 = _categoryService.TPassiveCategoryCount();
+            await Clients.All.SendAsync("ReceivePassiveCategoryCount", value4);
         }
     }
 }
