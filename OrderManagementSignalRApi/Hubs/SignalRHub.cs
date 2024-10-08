342db1e387c9131677570ed1a4ad2daea95d2872
Yunus Emre ÖNEÇ <107077148+yunusonec@users.noreply.github.com>
2024-10-08 20:01:02 +0300

    admin durumlar giriş

--- OrderManagementSignalRApi/Hubs/SignalRHub.cs
+++ OrderManagementSignalRApi/Hubs/SignalRHub.cs
@@ -8,11 +8,16 @@ namespace OrderManagementSignalRApi.Hubs
     {
         private readonly ICategoryService _categoryService;
         private readonly IProductService _productService;
-
-        public SignalRHub(ICategoryService categoryService, IProductService productService)
+        private readonly IOrderService _orderService;
+        private readonly IMoneyCaseService _moneyCaseService;
+        private readonly IMenuTableService _menuTableService;
+        public SignalRHub(ICategoryService categoryService, IProductService productService, IOrderService orderService, IMoneyCaseService moneyCaseService, IMenuTableService menuTableService)
         {
             _categoryService = categoryService;
             _productService = productService;
+            _orderService = orderService;
+            _moneyCaseService = moneyCaseService;
+            _menuTableService = menuTableService;
         }
 
         public async Task SendStatistic()
@@ -28,6 +33,42 @@ namespace OrderManagementSignalRApi.Hubs
 
             var value4 = _categoryService.TPassiveCategoryCount();
             await Clients.All.SendAsync("ReceivePassiveCategoryCount", value4);
+
+            var value5 = _productService.TProductCountByCategoryNameHamburger();
+            await Clients.All.SendAsync("ReceiveProductCountByCategoryNameHamburger", value5);
+
+            var value6 = _productService.TProductCountByCategoryNameDrink();
+            await Clients.All.SendAsync("ReceiveProductCountByCategoryNameDrink", value6);
+
+            var value7 = _productService.TProductPriceAvg(); 
+            await Clients.All.SendAsync("ReceiveProductPriceAvg", value7.ToString("0.00") + "₺");
+
+            var value8 = _productService.TProductNameByMaxPrice();
+            await Clients.All.SendAsync("ReceiveProductNameByMaxPrice", value8) ;
+
+            var value9 = _productService.TProductNameByMinPrice();
+            await Clients.All.SendAsync("ReceiveProductNameByMinPrice", value9);
+
+            var value10 = _productService.TProductAvgPriceByHamburger();
+            await Clients.All.SendAsync("Receiveproductavgpricebyhamburger", value10.ToString("0.00") + "₺");
+
+            var value11 = _orderService.TTotalOrderCount();
+            await Clients.All.SendAsync("ReceiveTotalOrderCount", value11);
+
+            var value12 = _orderService.TActiveOrderCount();
+            await Clients.All.SendAsync("ReceiveActiveOrderCount", value12);
+
+            var value13 = _orderService.TLastOrderPrice();
+            await Clients.All.SendAsync("ReceiveLastOrderPrice", value13.ToString("0.00") + "₺");
+
+            var value14 = _moneyCaseService.TTotalMoneyCaseAmount();
+            await Clients.All.SendAsync("ReceiveTotalMoneyCaseAmount", value14.ToString("0.00") + "₺");
+
+            var value15 = _orderService.TTodayTotalPrice();
+            await Clients.All.SendAsync("ReceiveTodayTotalPrice", value15.ToString("0.00") + "₺");
+
+            var value16 = _menuTableService.TMenuTableCount();
+            await Clients.All.SendAsync("ReceiveMenuTableCount", value16) ;
         }
     }
 }
