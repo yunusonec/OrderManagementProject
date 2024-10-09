c81510fbfc81cbe0bed7c0203af6f60597b5df15
Yunus Emre ÖNEÇ <107077148+yunusonec@users.noreply.github.com>
2024-10-09 16:31:00 +0300

    UI Başlangıcı

---
+++ OrderManagementSignalRWebUI/Controllers/ProgressBarsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace OrderManagementSignalRWebUI.Controllers
+{
+    public class ProgressBarsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
