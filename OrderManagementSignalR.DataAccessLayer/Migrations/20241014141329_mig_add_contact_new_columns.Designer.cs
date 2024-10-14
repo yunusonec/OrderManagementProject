3b4d17796e3eb3b7ee2ef54e9cc186f8393f7131
Yunus Emre ÖNEÇ <107077148+yunusonec@users.noreply.github.com>
2024-10-14 17:39:05 +0300

    Menü Sayfası Giriş

--- OrderManagementSignalR.DataAccessLayer/Migrations/20241014141329_mig_add_contact_new_columns.Designer.cs
+++ OrderManagementSignalR.DataAccessLayer/Migrations/20241014141329_mig_add_contact_new_columns.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using OrderManagementSignalR.DataAccessLayer.Concrete;
 
@@ -11,9 +12,10 @@ using OrderManagementSignalR.DataAccessLayer.Concrete;
 namespace OrderManagementSignalR.DataAccessLayer.Migrations
 {
     [DbContext(typeof(SignalRContext))]
-    partial class SignalRContextModelSnapshot : ModelSnapshot
+    [Migration("20241014141329_mig_add_contact_new_columns")]
+    partial class mig_add_contact_new_columns
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
