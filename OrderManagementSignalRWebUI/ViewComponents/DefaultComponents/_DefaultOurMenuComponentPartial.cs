a874931ea3425dc48647929133c1105e2894b412
Yunus Emre ÖNEÇ <107077148+yunusonec@users.noreply.github.com>
2024-10-14 16:47:25 +0300

    Hakkımızda kısmı ve referanslar kısmı api consumeleri

--- OrderManagementSignalRWebUI/ViewComponents/DefaultComponents/_DefaultOurMenuComponentPartial.cs
+++ OrderManagementSignalRWebUI/ViewComponents/DefaultComponents/_DefaultOurMenuComponentPartial.cs
@@ -1,12 +1,27 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using OrderManagementSignalRWebUI.Dtos.ProductDtos;
 
 namespace OrderManagementSignalRWebUI.ViewComponents.DefaultComponents
 {
     public class _DefaultOurMenuComponentPartial : ViewComponent
     {
-        public IViewComponentResult Invoke()
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public _DefaultOurMenuComponentPartial(IHttpClientFactory httpClientFactory)
         {
-            return View();
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:7005/api/Product");
+
+            var jsonData = await responseMessage.Content.ReadAsStringAsync();
+            var values = JsonConvert.DeserializeObject<List<ResultProductDto>>(jsonData);
+            return View(values);
+
         }
     }
 }
