9441408f07ae92cf30b8fc294acb1742ea417657
Yunus Emre ÖNEÇ <107077148+yunusonec@users.noreply.github.com>
2024-10-31 16:03:26 +0300

    Chat Js İlk Kısım

---
+++ OrderManagementSignalRWebUI/Controllers/MessageController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace OrderManagementSignalRWebUI.Controllers
+{
+    public class MessageController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
